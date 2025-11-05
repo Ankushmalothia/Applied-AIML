5dc2063ad88085fe56e55801b1d6871ba0e48525
Ankushmalothia <Ankushmalothia@gmail.com>
2025-11-06 00:49:55 +0530

    Changing the width of bars

--- Learning Matplotlib/Initial use of Matplotlib.py
+++ Learning Matplotlib/Initial use of Matplotlib.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 product_catagory = np.array(['furniture', 'technology', 'office supplies'])
 sales = np.array([4110451.90, 4744557.50, 3787492.52])
 
-plt.bar(product_catagory, sales, color = "green", edgecolor = "red")
+plt.bar(product_catagory, sales, color = "green", edgecolor = "red", width = 0.5)
 
 plt.title("Sales graph", fontdict={"fontsize" : 20, "fontweight" : 3, "color" : "Gray"})
 plt.xlabel("Product Catagory", fontdict={"fontsize" : 15, "fontweight" : 2, "color" : "Gray"})
