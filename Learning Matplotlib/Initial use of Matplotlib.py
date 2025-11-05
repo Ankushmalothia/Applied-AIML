8548f439cb5427ce694f7fc8275e223293bfbb4c
Ankushmalothia <Ankushmalothia@gmail.com>
2025-11-06 00:15:29 +0530

    Making title, changing colour, font, and weight(making bold)

--- Learning Matplotlib/Initial use of Matplotlib.py
+++ Learning Matplotlib/Initial use of Matplotlib.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 product_catagory = np.array(['furniture', 'technology', 'office supplies'])
 sales = np.array([4110451.90, 4744557.50, 3787492.52])
 
-graph = plt.bar(product_catagory, sales)
+plt.bar(product_catagory, sales)
+plt.title("Sales graph", fontdict={"fontsize" : 20, "fontweight" : 3, "color" : "Gray"})
+plt.xlabel("Product Catagory", fontdict={"fontsize" : 15, "fontweight" : 2, "color" : "Gray"})
+plt.ylabel("Sales", fontdict={"fontsize" : 15, "fontweight" : 2, "color" : "Gray"})
 
 plt.show()
