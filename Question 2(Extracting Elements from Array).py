89502639b2d024115057e1b31740a2e9deb779f6
Ankushmalothia <Ankushmalothia@gmail.com>
2025-11-05 17:01:03 +0530

    Did question 2 again

--- Question 2(Extracting Elements from Array).py
+++ Question 2(Extracting Elements from Array).py
@@ -1,11 +1,12 @@
 import ast 
-input_list=ast.literal_eval(input())
-m=int(input())
-n=int(input())
+input_list = ast.literal_eval(input())
+m = int(input())
+n = int(input())
 
 import numpy as np
 array_1 = np.array(input_list)
-final_array = array_1[m:n+1]
+
+# filter values strictly between m and n
+final_array = array_1[(array_1 > m) & (array_1 < n)]
 
 print(final_array)
-
