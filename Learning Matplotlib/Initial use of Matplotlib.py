73ebd81491666bd22edb3810590a5f223207c90c
Ankushmalothia <Ankushmalothia@gmail.com>
2025-11-06 00:43:42 +0530

    Scaling and renaming the values along the y axis

--- Learning Matplotlib/Initial use of Matplotlib.py
+++ Learning Matplotlib/Initial use of Matplotlib.py
@@ -5,8 +5,13 @@ product_catagory = np.array(['furniture', 'technology', 'office supplies'])
 sales = np.array([4110451.90, 4744557.50, 3787492.52])
 
 plt.bar(product_catagory, sales, color = "green", edgecolor = "red")
+
 plt.title("Sales graph", fontdict={"fontsize" : 20, "fontweight" : 3, "color" : "Gray"})
 plt.xlabel("Product Catagory", fontdict={"fontsize" : 15, "fontweight" : 2, "color" : "Gray"})
 plt.ylabel("Sales", fontdict={"fontsize" : 15, "fontweight" : 2, "color" : "Gray"})
+
+tick_values = np.arange(0, 7000000, 1000000)
+tick_labels = ["0L", "10L", "20L", "30L", "40L", "50L", "60L"]
+plt.yticks(tick_values, tick_labels)
 
 plt.show()
